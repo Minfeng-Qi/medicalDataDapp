54ad16254fe5f83d2f87ecf679553e1385ca6be7
GuoyiShi97 <D23091101564@cityu.edu.mo>
2024-08-17 21:07:59 +0800

    index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors'); // Import the cors package
-const ipfsRouter = require('./routes/ipfs');
-
+const cors = require('cors');
 const app = express();
-const port = 5001;
+const port = 5000;
 
-app.use(cors()); // Enable CORS for all routes
-app.use(bodyParser.json());
-app.use('/api/ipfs', ipfsRouter);
+// 使用CORS中间件
+app.use(cors());
+
+// 解析JSON请求体
+app.use(express.json());
+
+// 路由
+const userRouter = require('./routes/user');
+const medicalDataRouter = require('./routes/medicalData'); // 确保导入了medicalData路由
+const policyRouter = require('./routes/policy');
+const accessControlRouter = require('./routes/accessControl');
+
+app.use('/api/user', userRouter);
+app.use('/api/medicalData', medicalDataRouter); // 确保正确使用了路由
+app.use('/api/policy', policyRouter);
+app.use('/api/accessControl', accessControlRouter);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
